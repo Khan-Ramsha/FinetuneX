fix: proper label masking in data collator to prevent NaN loss

data_collator.py
@@ -13,8 +13,8 @@ class DataCollator:
        padded_ids = [F.pad(ids, (0, max_len - len(ids)), value = self.pad_token_id) for ids in input_ids]
        attention_mask = [torch.ones(len(ids), dtype=torch.long) for ids in input_ids]
        attention_mask = [F.pad(mask, (0, max_len - len(mask)), value = 0) for mask in attention_mask] #putting zeros where attention not needed!
-       labels = [ids.clone() for ids in input_ids]
-       padded_labels = [F.pad(l, (0, max_len - len(l)), value = -100) for l in labels]
+       
+       padded_labels = [ids.clone() for ids in padded_ids]
 
        #calculate loss for only completion
        if self.completion_only_loss and "completion_mask" in examples[0]:
@@ -22,6 +22,11 @@ class DataCollator:
                mask = torch.tensor(e["completion_mask"], dtype = torch.bool)
                mask = F.pad(mask, (0, max_len - len(mask)), value = 0) #zeros for padding
                padded_labels[i][~mask] = -100
+               
+       for i, ids in enumerate(padded_labels):
+            pad_mask = (padded_ids[i] == self.pad_token_id)
+            padded_labels[i][pad_mask] = -100
+
        # Final batch dict
        output = {
           "input_ids": torch.stack(padded_ids),
@@ -31,16 +36,16 @@ class DataCollator:
        return output
        #returns inputs ids, labels, attention masks (here completion only loss and completion mask is used)
 
-# collator = DataCollator(pad_token_id = 0, completion_only_loss = True)
-# example = [
-#     {
-#         "input_ids": [101, 2023, 2003, 2307],  
-#         "completion_mask": [0, 0, 1, 1],  
-#     },
-#     {
-#         "input_ids": [101, 2009, 2003, 1037, 2204, 2154],
-#         "completion_mask": [0, 0, 0, 1, 1, 1]
-#     }
-# ]
-# result = collator(example)
-# print(result)
+collator = DataCollator(pad_token_id = 0, completion_only_loss = True)
+example = [
+    {
+        "input_ids": [101, 2023, 2003, 2307],  
+        "completion_mask": [0, 0, 1, 1],  
+    },
+    {
+        "input_ids": [101, 2009, 2003, 1037, 2204, 2154],
+        "completion_mask": [0, 0, 0, 1, 1, 1]
+    }
+]
+result = collator(example)
+print(result)