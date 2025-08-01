6a509f3d89e8636126d7dc681bea40ec5b5d88ea
Khan-Ramsha <khanramsha302020@gmail.com>
2025-08-02 01:30:38 +0530

    Add Root Mean square Normalization

--- modules/norm.py
+++ modules/norm.py
@@ -1,1 +1,18 @@
-"""RMSNorm"""
+"""
+RMSNorm
+formula: ai' = ai/RMS(a) * gi 
+         RMS(a) = sqrt(1/n summation of ai^2)
+         (g is a trainable/learnable vector)
+"""
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, hidden_size: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps #prevents division by zero
+        self.weight = nn.Parameter(torch.ones(hidden_size)) # 'g' learnable vector using Parameter from torch - enable gradient computation during backward pass
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        rms = x.pow(2).mean(dim=-1,keepdim=True).sqrt()
+        return self.weight * (x / (rms + self.eps))
