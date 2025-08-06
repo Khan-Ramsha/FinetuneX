3238bf9655e5f3446227d1f447ca4b9a506fc967
Khan-Ramsha <khanramsha302020@gmail.com>
2025-08-06 15:55:22 +0530

    KV cache & causal masking

--- modules/attention.py
+++ modules/attention.py
@@ -15,7 +15,7 @@ class MultiHeadAttention(nn.Module):
         self.to_v = nn.Linear(dim, dim,bias=True)
         self.output_proj = nn.Linear(dim, dim,bias=True)
     
-    def forward(self, x):
+    def forward(self, x, past_kv = None):
         B, T, D = x.shape
         q = self.to_q(x) #linear transformation, x@Wq + b (Wq learnable weight for query)
         k = self.to_k(x)
@@ -25,12 +25,21 @@ class MultiHeadAttention(nn.Module):
         q = q.view(B, -1, self.num_heads, self.headD).transpose(1, 2)
         k = k.view(B, -1, self.num_heads, self.headD).transpose(1, 2)
         v = v.view(B, -1, self.num_heads, self.headD).transpose(1, 2)
+        
+        present_kv = (k,v)
+        if past_kv is not None:
+            past_k, past_v = past_kv
+            #Concatenate 
+            k = torch.cat((past_k, k), dim = 2)
+            v = torch.cat((past_v, v), dim = 2)
+            present_kv = (k,v)
+
         #scaled dot-product
         att_scores = (q @ k.transpose(-2, -1)) / math.sqrt(self.headD)
-        scores = F.softmax(att_scores, dim = -1)
-        att_scores = scores @ v
+        mask = torch.tril(torch.ones(T,T)).unsqueeze(0).unsqueeze(0)
+        att_scores = att_scores.masked_fill_(mask == 9, float("-inf"))
+        att_probs = F.softmax(att_scores, dim = -1)
+        att_scores = att_probs @ v
         output = att_scores.transpose(1, 2).contiguous().view(B, T, D)
         output = self.output_proj(output)
-        return output
-
-    
+        return output, present_kv
