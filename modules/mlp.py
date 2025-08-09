355e064b9dc0d5d0de725e78e80ac3fff079b04c
Khan-Ramsha <khanramsha302020@gmail.com>
2025-08-10 01:11:36 +0530

    add MLP with SwiGLU activation function

--- modules/mlp.py
+++ modules/mlp.py
@@ -1,1 +1,17 @@
 """SwiGLU (activation function) in MLP"""
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, hidden_size, intermediate_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size 
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size)
+        self.silu = nn.SiLU()
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size)
+
+    def forward(self, x):
+        x = self.down_proj(self.silu(self.gate_proj(x)) * self.up_proj(x))
+        return x
