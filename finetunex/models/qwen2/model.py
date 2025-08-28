fix architecture (subtle changes in RMS, RoPE, Attention, computes self implemented scaled dot product & causal masking)

finetunex/models/qwen2/model.py
@@ -17,7 +17,7 @@ class DecoderBlock(nn.Module):
         self.post_attention_layernorm = RMSNorm(config.hidden_size, config.rms_norm_eps)
         self.mlp = MLP(config)
     
-    def forward(self, x,pos_emb=None, attention_mask = None):
+    def forward(self, x, pos_emb, attention_mask):
         x = x + self.attn(self.input_layernorm(x), pos_emb, attention_mask)
         x = x + self.mlp(self.post_attention_layernorm(x))
         return x
@@ -45,17 +45,21 @@ class Qwen2Model(BaseModel):
     def decoder_layer(self, config, layer_idx):
         return DecoderBlock(config, layer_idx)
     
-    def forward(self, input_ids, attention_mask = None, labels = None):
+    def forward(self, input_ids, labels = None):
         B, T = input_ids.shape
         hidden_states = self.embed_tokens(input_ids) 
         position_ids = torch.arange(T, device=input_ids.device).unsqueeze(0).expand(B, -1)
         pos_emb = self.rotary(hidden_states, position_ids)
+        
+        num_tokens = hidden_states.shape[1]
+        attention_mask = torch.triu(torch.ones(num_tokens, num_tokens, device=hidden_states.device, dtype=torch.bool), diagonal=1)
+        attention_mask = attention_mask[None, None, :, :] #broadcasting
         # passing hidden states to stack of blocks
         for layer in self.layers:
-            hidden_states = layer(hidden_states,pos_emb, attention_mask)
+            hidden_states = layer(hidden_states, pos_emb, attention_mask)
         hidden_states = self.norm(hidden_states)
         logits = self.lm_head(hidden_states)
-        
+
         loss = None
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss(ignore_index=-100)

finetunex/modules/attention.py
@@ -24,7 +24,7 @@ class GroupQueryAttention(nn.Module):
         self.v_proj = nn.Linear(dim, self.headD * self.num_head_kv, bias=True)
         self.o_proj = nn.Linear(self.headD * self.num_head_q, dim, bias=False)
 
-    def forward(self, x, position_emb,  attention_mask = None):
+    def forward(self, x, position_emb,  attention_mask):
         B, T, D = x.shape
         q = self.q_proj(x) #linear transformation, x@Wq + b (Wq learnable weight for query)
         k = self.k_proj(x)
@@ -42,27 +42,8 @@ class GroupQueryAttention(nn.Module):
         # repeating kv heads to match query head
         k = k.repeat_interleave(self.num_head_q // self.num_head_kv, dim = 1)
         v = v.repeat_interleave(self.num_head_q // self.num_head_kv, dim = 1)
-
-        # # causal masking
-        # causal_mask = attention_mask
-        # if attention_mask is not None: #slicing
-        #     causal_mask = attention_mask[:, :, :, : k.shape[-2]]
-        if attention_mask is not None:
-            attention_mask = attention_mask.to(dtype = q.dtype, device = q.device)
-        #handling non-contiguity of tensors on cuda
-        if q.device.type == "cuda" and attention_mask is not None:
-            q = q.contiguous()
-            k = k.contiguous()
-            v = v.contiguous()
-        # if attention mask not provided & not during inference, put causal = True
-        is_causal = True if attention_mask is None and T > 1 else False
-        att_output = F.scaled_dot_product_attention(
-            q,
-            k,
-            v,
-            attn_mask=attention_mask,
-            is_causal=is_causal
-        )
-        output = att_output.transpose(1, 2).contiguous().view(B, T, D)
-        output = self.o_proj(output) #linear transformation to extract useful info from output 
-        return output
+        attn_scores = q @ k.transpose(2, 3)  # (b, n_heads, q_len, k_len)
+        attn_scores = attn_scores.masked_fill(attention_mask, float("-inf"))
+        attn_weights = torch.softmax(attn_scores / self.headD **0.5, dim=-1)
+        context = (attn_weights @ v).transpose(1, 2).reshape(B, T, self.headD * self.num_head_q)
+        return self.o_proj(context)

finetunex/modules/norm.py
@@ -14,5 +14,8 @@ class RMSNorm(nn.Module):
         self.weight = nn.Parameter(torch.ones(hidden_size)) # 'g' learnable vector using Parameter from torch - enable gradient computation during backward pass
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        rms = x.pow(2).mean(dim=-1,keepdim=True).sqrt()
-        return self.weight * (x / (rms + self.eps))
+        x_type = x.dtype
+        x = x.to(torch.float32)
+        variance = x.pow(2).mean(-1, keepdim=True)
+        norm_x = x * torch.rsqrt(variance + self.eps)
+        return self.weight * norm_x.to(x_type)

finetunex/modules/positional_encoding.py
@@ -12,35 +12,36 @@ class RotaryEmbedding(nn.Module):
         super().__init__()
         self.dim = dim
         self.rope_theta = rope_theta # base frequency
-        inv_freq = 1.0 / (self.rope_theta ** (torch.arange(0, dim, 2).float() / dim))
+        inv_freq = 1.0 / (self.rope_theta ** (torch.arange(0, dim, 2, dtype=torch.float, device="cuda") / dim))
         self.register_buffer("inv_freq", inv_freq)
 
     def forward(self, x, position_ids = None):
         *p, seq_len, dim = x.shape
         inv_freq = self.inv_freq
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1).to(x.device)
         if position_ids is not None:
             # Use provided position_ids
-            if position_ids.dim() > 1:
-                t = position_ids[0].float().type_as(inv_freq) 
-            else: 
-                t = position_ids.float().type_as(inv_freq) 
+            position_ids_expanded = position_ids[:, None, :].float()
         else:
-            t = torch.arange(seq_len, device=x.device).type_as(inv_freq) # positions
-        freqs = torch.einsum('i,j->ij', t, inv_freq) # t @ inv_freq (matmul)
-        emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-        cos = emb.cos()
-        sin = emb.sin()
-        cos = cos[None, None, :, :]
-        sin = sin[None, None, :, :] # to match [B, H, seq_len, head_dim]
+            position_ids_expanded = torch.arange(seq_len, device=x.device).type_as(inv_freq) # positions
+        device_type = x.device.type if isinstance(x.device.type, str) and x.device.type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):  # Force float32
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            cos = emb.cos()
+            sin = emb.sin()
         return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
     
 """rotating key values"""
 def rotate_half(x):
-    x1 = x[..., : x.shape[-1] // 2]
+    #split into first and second half
+    x1 = x[..., : x.shape[-1] // 2] 
     x2 = x[..., x.shape[-1] // 2 :]
     return torch.cat((-x2, x1), dim = -1)
 
 def apply_rotary_emb(q, k, cos, sin):
+    cos = cos.unsqueeze(1) #shape: [batch_size, :, seq_len, head_dim] making compatible to q & v shape
+    sin = sin.unsqueeze(1)
     q_emb = (q * cos) + (rotate_half(q) * sin)
     k_emb = (k * cos) + (rotate_half(k) * sin)
     return q_emb, k_emb

finetunex/text_generation/generate.py
@@ -1,24 +1,17 @@
 import torch
 from finetunex.text_generation.decoding import stochastic_sampling
 
-def generate(model, inputs, max_new_tokens, top_p, top_k, temperature, stop_tokens):
-    if inputs.dim() == 1:
-        inputs = inputs.unsqueeze(0)
-    batch, input_len = inputs.shape
-    max_tokens = model.config.max_position_embeddings
-    max_new_tokens = min(max_new_tokens, max_tokens - input_len - 10)
-    curr_sequence = inputs
-    generated = []
-    for _ in range(max_new_tokens):
-        if curr_sequence.size(1) >= max_tokens - 1:
-            keep_len = max_tokens // 2
-            curr_sequence = curr_sequence[:, -keep_len:]
+def generate(model, idx, max_new_tokens, context_size, temperature ,top_k, top_p, stop_tokens):
+    model.eval()
+    ctx_len = context_size
 
-        output = model(curr_sequence)
-        logits = output['logits'][:, -1, :]
-        next_token = stochastic_sampling(logits, temperature, top_k, top_p)
-        if next_token.item() in stop_tokens:
-            break
-        generated.append(next_token.item())
-        curr_sequence = torch.cat([curr_sequence, next_token], dim=1)
-    return generated
+    with torch.no_grad():
+        for _ in range(max_new_tokens):
+            model_input = idx[:, -ctx_len:] if idx.size(1) > ctx_len else idx
+            output = model(model_input)
+            logits = output['logits']
+            next_idx = stochastic_sampling(logits, temperature, top_k, top_p)
+            idx = torch.cat([idx, next_idx], dim=1)
+            if next_idx.item() in stop_tokens:
+                break
+    return idx

inference.py
@@ -27,21 +27,23 @@ def infer(prompt, model_path, model_name):
     )
     
     inputs = tokenizer(chat_input, return_tensors="pt").to(device)
-    inputs = inputs["input_ids"]
+    prompt = inputs["input_ids"]
     max_new_tokens = 250
     
     with torch.no_grad():
-        outputs = generate(
+        generated_tokens = generate(
             model,
-            inputs,
-            max_new_tokens=max_new_tokens,
-            top_p=0.8,  # Slightly higher for more diversity
-            top_k=50,   # Lower for more focused responses
-            temperature=0.7,  # More balanced temperature
+            prompt,
+            max_new_tokens = max_new_tokens,
+            context_size = model.config.max_position_embeddings,
+            temperature = 0.7, 
+            top_k = 50,  
+            top_p = 0.8, 
             stop_tokens=[tokenizer.eos_token_id]  # Pass as list, not tuple
         )
-    response = tokenizer.decode(outputs)
-    return response
+    generated_text = tokenizer.decode(generated_tokens[0].tolist())
+    response = generated_text.split('<|im_start|>assistant\n')[-1]
+    return response.strip()
 
 def infer_base(prompt, model_path):
     print("\n" + "="*50)