a2d3a7d6ce4b7c82bf5ea59261b4c9b88f056191
Khan-Ramsha <khanramsha302020@gmail.com>
2026-06-10 21:10:41 +0530

    chore: minor changes

--- build_dataset.py
+++ build_dataset.py
@@ -5,6 +5,7 @@ from datasets import load_dataset
 def create_dataset(name):
     data = load_dataset(name, "main")
     df = pd.DataFrame(data["train"])
+    df = df.select(range(min(300, len(data["train"]))))
     data = ChatMLPreprocessor(df).convert_to_chatml()
     split = data.train_test_split(
         test_size=0.2,
