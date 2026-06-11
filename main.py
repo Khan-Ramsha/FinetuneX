1e7d94478f6f8f9c273347db8809c31ef5b89d3b
Khan-Ramsha <khanramsha302020@gmail.com>
2026-06-11 13:21:11 +0530

    add inference

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main(rank: int, world_size: int, model_name: str, dataset: str, training_arg
             world_size=world_size,
         )
         if rank == 0:
-            model_path = f"{training_args.output_dir}/checkpoint_epoch_{training_args.epoch-1}"
+            model_path = f"{training_args.output_dir}/checkpoint_epoch_{training_args.epochs-1}"
             prompt = input("Enter ")
             result = infer(prompt, model_path, model_name)
             print("f\n== Model Response == \n {result}")
